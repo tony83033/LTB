342a05e02f146d88e2a95800495af82cc435aeeb
Sumit pal <tonystark83033@gmail.com>
2023-06-01 01:54:22 +0530

    add Services button in navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -68,12 +68,12 @@ const Navbar = () => {
             </ul>
           </div>
           <div className="hidden lg:block">
-            {/* <button
+            <button
               type="button"
               className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
             >
-              Button text
-            </button> */}
+              Services
+            </button>
           </div>
           <div className="lg:hidden">
             <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer" />
