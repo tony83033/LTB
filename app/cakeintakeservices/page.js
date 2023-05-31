2df158c49843f6703f8b740894870200f245b984
Sumit pal <tonystark83033@gmail.com>
2023-06-01 03:57:48 +0530

    build a new component of cakeintakeservices components/services

---
+++ app/cakeintakeservices/page.js
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import Description from '@/components/services/cakeintakeservices/description'
+
+
+const cakeintakeservices = () => {
+ 
+
+  return (
+    <React.Fragment>
+    <Description></Description>
+    </React.Fragment>
+  )
+}
+export default cakeintakeservices;
---
+++ components/services/cakeintakeservices/description.js
@@ -0,0 +1,412 @@
+import React from 'react'
+
+const description = () => {
+  return (
+    <React.Fragment>
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-8 mx-auto">
+    <div className="flex flex-col text-center w-full mb-20">
+      <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
+      Case Intake Services
+
+      </h2>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+      Description
+      </h1>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+      The Legal Base LPO is a leading legal intake service provider and has helped several attorneys and practices grow their businesses and be successful in the competitive marketplace. We provide high-quality legal intake solutions that are customised to your exact needs and requirements.
+
+      </p>
+      <hr></hr>
+      <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+      Businesses may need case intake services to manage and organise potential clients in a streamlined and efficient manner, ensure compliance with laws and regulations, improve the overall quality of service provided to clients, collect and manage client data, save time and money, and ensure that services are accessible to a wider range of clients.
+
+
+      </p>
+    </div> 
+  </div>
+</section>
+{/* ================================================= */}
+
+<section className="text-gray-600 body-font">
+  <div className="container px-5 py-0 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="p-4 lg:w-1/3">
+        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          Case Intake Services
+
+          </h2>
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+          Efficiency
+          </h1>
+          <p className="leading-relaxed mb-3">
+          Case intake services can help a business manage and organize potential clients in a streamlined and efficient manner. By having a dedicated intake process in place, a business can quickly assess client needs and determine their eligibility for services.
+
+          </p>
+          <a className="text-indigo-500 inline-flex items-center">
+            Learn More
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5l7 7-7 7" />
+            </svg>
+          </a>
+          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                <circle cx={12} cy={12} r={3} />
+              </svg>
+              1.2K
+            </span>
+            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+              </svg>
+              6
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 lg:w-1/3">
+        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          Case Intake Services
+          </h2>
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+          Compliance
+          </h1>
+          <p className="leading-relaxed mb-3">
+          Certain industries, such as healthcare and legal, are regulated and may require case intake processes to be in place to ensure compliance with laws and regulations.
+
+          </p>
+          <a className="text-indigo-500 inline-flex items-center">
+            Learn More
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5l7 7-7 7" />
+            </svg>
+          </a>
+          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                <circle cx={12} cy={12} r={3} />
+              </svg>
+              1.2K
+            </span>
+            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+              </svg>
+              6
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 lg:w-1/3">
+        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          Case Intake Services
+          </h2>
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+          Quality assurance
+          </h1>
+          <p className="leading-relaxed mb-3">
+          By having a case intake process in place, a business can ensure that clients receive the appropriate level of service and are matched with the right case manager or service provider. This can help improve the overall quality of service provided to clients.
+
+          </p>
+          <a className="text-indigo-500 inline-flex items-center">
+            Learn More
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5l7 7-7 7" />
+            </svg>
+          </a>
+          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                <circle cx={12} cy={12} r={3} />
+              </svg>
+              1.2K
+            </span>
+            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+              </svg>
+              6
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div className="container px-5 py-0 mx-auto">
+    <div className="flex flex-wrap -m-4">
+      <div className="p-4 lg:w-1/3">
+        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          Case Intake Services
+
+          </h2>
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+          Data collection and management
+          </h1>
+          <p className="leading-relaxed mb-3">
+          Case intake services can help a business collect and manage client data, which can be used for research, analysis, and reporting.
+
+
+          </p>
+          <a className="text-indigo-500 inline-flex items-center">
+            Learn More
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5l7 7-7 7" />
+            </svg>
+          </a>
+          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                <circle cx={12} cy={12} r={3} />
+              </svg>
+              1.2K
+            </span>
+            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+              </svg>
+              6
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 lg:w-1/3">
+        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          Case Intake Services
+          </h2>
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+          Cost-effective
+          </h1>
+          <p className="leading-relaxed mb-3">
+          A well-structured case intake process can save a business time, money and effort by avoiding unnecessary intake, avoiding missed opportunities and by providing a cost-effective service.
+
+          </p>
+          <a className="text-indigo-500 inline-flex items-center">
+            Learn More
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5l7 7-7 7" />
+            </svg>
+          </a>
+          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                <circle cx={12} cy={12} r={3} />
+              </svg>
+              1.2K
+            </span>
+            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+              </svg>
+              6
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="p-4 lg:w-1/3">
+        <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+          <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          Case Intake Services
+          </h2>
+          <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+          Accessibility
+          </h1>
+          <p className="leading-relaxed mb-3">
+          Case intake services can help a business ensure that its services are accessible to a wider range of clients by providing language and accessibility accommodations.
+
+
+          </p>
+          <a className="text-indigo-500 inline-flex items-center">
+            Learn More
+            <svg
+              className="w-4 h-4 ml-2"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 12h14" />
+              <path d="M12 5l7 7-7 7" />
+            </svg>
+          </a>
+          <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+            <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                <circle cx={12} cy={12} r={3} />
+              </svg>
+              1.2K
+            </span>
+            <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+              <svg
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+                strokeWidth={2}
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+              >
+                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+              </svg>
+              6
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+    </React.Fragment>
+  )
+}
+
+export default description
