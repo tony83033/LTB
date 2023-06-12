1cb989ea551687cad39c7eeb91c92ced9b1ef490
Sumit pal <tonystark83033@gmail.com>
2023-06-12 21:19:31 +0530

    paralegal services

---
+++ components/paralegalservices/second.js
@@ -0,0 +1,135 @@
+import React from 'react'
+
+const Second = () => {
+  return (
+   <React.Fragment>
+    <section className="dark:bg-gray-800 dark:text-gray-100">
+	<div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
+		<div>
+			<h2 className="text-xl font-semibold tracking-tight text-center sm:text-5xl dark:text-gray-50 ">Here are some common paralegal services which are typically outsourced to TLB:
+</h2>
+			{/* <p className="max-w-3xl mx-auto mt-4 text-xl text-center dark:text-gray-400">Here are some common paralegal services which are typically outsourced to TLB.</p> */}
+		</div>
+		<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+			<div>
+				<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">Legal Research: 
+</h3>
+				<p className="mt-3 text-lg dark:text-gray-400">TLB Paralegals can conduct legal research on specific issues, case law, statutes, and regulations. They can gather relevant information, analyze legal precedents, and provide summaries or briefs to assist attorneys in their case preparation.
+</p>
+				<div className="mt-12 space-y-12">
+					<div className="flex shadow-lg p-4 hover:shadow-2xl hover:p-4 border-solid border-4 border-black-900 rounded-md">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-[100] rounded-md dark:bg-violet-400 dark:text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Document Preparation and Review: 
+</h4>
+							<p className="mt-2 dark:text-gray-400">TLB Paralegals can assist with drafting, reviewing, and proofreading legal documents such as contracts, agreements, pleadings, motions, and correspondence. They ensure documents comply with legal formatting and assist with the organization and management of document databases.
+</p>
+						</div>
+					</div>
+					<div className="flex shadow-lg p-4 hover:shadow-2xl hover:p-4 border-solid border-4 border-black-900 rounded-md">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Case Management: 
+</h4>
+							<p className="mt-2 dark:text-gray-400">TLB Paralegals can help manage case files, organize documents, and evidence, and maintain electronic or physical filing systems. They may assist in preparing trial notebooks, exhibit lists, and witness binders, and support attorneys during hearings, depositions, or trials.
+</p>
+						</div>
+					</div>
+					<div className="flex shadow-lg p-4 hover:shadow-2xl hover:p-4 border-solid border-4 border-black-900 rounded-md">
+						<div className="flex-shrink-0">
+							<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+								</svg>
+							</div>
+						</div>
+						<div className="ml-4">
+							<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Discovery Support: 
+</h4>
+							<p className="mt-2 dark:text-gray-400">TLB Paralegals can assist with the discovery process, including drafting and responding to discovery requests, organizing, and managing document productions, and preparing privilege logs. They may also assist with e-discovery, utilizing specialized software and tools.
+</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div aria-hidden="true" className="mt-10 lg:mt-0">
+				<img src="https://source.unsplash.com/random/360x480" alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
+			</div>
+		</div>
+		<div>
+			<div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
+				<div className="lg:col-start-2">
+					<h3 className="text-2xl font-bold tracking-tight sm:text-3xl dark:text-gray-50">Contract Management: 
+</h3>
+					<p className="mt-3 text-lg dark:text-gray-400">TLB Paralegals can help with contract management tasks, such as reviewing contracts, identifying key terms and risks, maintaining contract databases, and tracking contract deadlines and obligations.
+</p>
+					<div className="mt-12 space-y-12">
+						<div className="flex shadow-lg p-4 hover:shadow-2xl hover:p-4 border-solid border-4 border-black-900 rounded-md">
+							<div className="flex-shrink-0">
+								<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+									</svg>
+								</div>
+							</div>
+							<div className="ml-4">
+								<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Corporate Governance Support: 
+</h4>
+								<p className="mt-2 dark:text-gray-400">TLB Paralegals can provide assistance with corporate governance matters, including preparing board meeting minutes, drafting resolutions, maintaining corporate records, and assisting with compliance-related tasks.</p>
+							</div>
+						</div>
+						<div className="flex shadow-lg p-4 hover:shadow-2xl hover:p-4 border-solid border-4 border-black-900 rounded-md">
+							<div className="flex-shrink-0">
+								<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+									</svg>
+								</div>
+							</div>
+							<div className="ml-4">
+								<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Intellectual Property Support: 
+</h4>
+								<p className="mt-2 dark:text-gray-400">TLB Paralegals with expertise in intellectual property law can assist with tasks such as trademark searches, patent filings, copyright registrations, and maintaining intellectual property portfolios.
+</p>
+							</div>
+						</div>
+						<div className="flex shadow-lg p-4 hover:shadow-2xl hover:p-4 border-solid border-4 border-black-900 rounded-md">
+							<div className="flex-shrink-0">
+								<div className="flex items-center justify-center w-12 h-12 rounded-md dark:bg-violet-400 dark:text-gray-900">
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-7 h-7">
+										<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+									</svg>
+								</div>
+							</div>
+							<div className="ml-4">
+								<h4 className="text-lg font-medium leading-6 dark:text-gray-50">Immigration Support: </h4>
+								<p className="mt-2 dark:text-gray-400">TLB Paralegals can provide support in immigration matters, including completing immigration forms, gathering supporting documentation, and assisting with visa applications or petitions.
+</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
+					<img src="https://source.unsplash.com/random/361x481" alt="" className="mx-auto rounded-lg shadow-lg dark:bg-gray-500" />
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+   </React.Fragment>
+  )
+}
+
+export default Second;
