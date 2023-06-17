0c13e7d89273898852e6228bf89e4e7e11bffca9
Sumit pal <tonystark83033@gmail.com>
2023-06-17 21:05:40 +0530

    add flip card

--- components/paralegalservices/five.js
+++ components/paralegalservices/five.js
@@ -1,133 +1,184 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import React from 'react';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Image from 'next/image';
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/effect-coverflow";
-import "swiper/css/pagination";
+const Five = () => {
+  useEffect(() => {
+    AOS.init({
+      offset: 300,
+      delay: 0,
+      duration: 1000,
+    });
+  }, []);
 
-import "./styles.css";
-
-// import required modules
-import {Autoplay, EffectCoverflow, Pagination } from "swiper";
-
-export default function Five() {
-    const cardData = [
-        {
-          title: "Case Management",
-          description: "Reviewing, organizing, indexing, and summarizing client files\nPreparing different correspondence\nPreparing a variety of pleadings"
-        },
-        {
-          title: "Discovery Preparation",
-          description: "Drafting Subpoenas, Interrogatories, and Discovery Demands\nDetailed Analysis Reports of Depositions and Interrogatories\nWitness Summary\nRequesting for Medical Reports and Detailed Analysis of Medical Reports"
-        },
-        {
-          title: "Trial Preparation",
-          description: "Expert witness report summarization\nDocument summarization for discovery\nProperty search from public records\nDocument Management with the use of software Casemap"
-        },
-        {
-          title: "Legal Document Production",
-          description: "Contracts\nPower of attorneys\nPrenuptial agreements\nCase Information statements\nProperty Settlement agreements"
-        },
-        {
-          title: "Editorial Support",
-          description: "Abstraction of case law and legal journals\nIndexing legal material\nLegal research\nCopy editing"
-        },
-        {
-          title: "Organizing Legal Database",
-          description: "Creating legal databases for law firms and corporates\nProviding assistance in organizing the information into an index and searchable database"
-        },
-        {
-          title: "Proofreading Services",
-          description: "Reviewing and rectifying spellings\nMaking grammatical changes if needed\nConfirming uniformity in typefaces throughout the document\nDetermining accurate and consecutive numbering of several elements like page numbers\nLooking for errors in formatting\nValidating table of contents with elements like titles and subtitles\nValidating the appropriateness of in-text illustrations\nReal Estate Title Search\nData Entry Services"
-        },
-        {
-          title: "Corporate Services",
-          description: "Coordinating Hearings, Depositions, and Meetings with Clients\nPreparing Notices for Hearings and Depositions\nArranging Court Reporters and Ordering Transcripts\nFormatting and Finalizing Pleadings and Correspondence\nFiling Documents Electronically\nPreparing Periodic Client Invoices\nPerforming Clerical Tasks and Assignments\nPreparing Table of Authorities\nPreparing Table of Contents\nLegal Typing and Creating Minutes of Meetings"
-        },
-        {
-          title: "Legal Research and Drafting Services",
-          description: "Performing Due Diligence\nData Room Management\nAdministrative and Regulatory Law Research Services\nLegislation Research Services, including Primary (Regulations, Statutes, Cases, etc.) and Secondary Sources (Legal Dictionaries, Law Reviews, Treatises, etc.)\nCase Law Research Services\nCompliance Research Services\nMulti-Jurisdictional Research Services\nMemoranda of Law Research Services\nDrafting of agreements/contracts as well as court-specific documents\nAnalyzing SEC Forms such as Form 4 and inputting the relevant information into Excel."
-        },
-        {
-          title: "Legal Transcription Services",
-          description: "Transcribing audio files related to legal cases.\nTribunal Transcription\nVerbatim Voice Dictation"
-        },
-        {
-          title: "Litigation Support Services",
-          description: "Depositions\nMedical Records\nDocument Review Management\nElectronic Data Discovery Solutions\nProcessing of Information Stored Electronically"
-        },
-        {
-          title: "Other services",
-          description: "Data Management\nReviewing documentation\nLegal data entry services\nBusiness registrations\nPreparing Agreements and Contracts (business and other kind)\nData format conversion and storage\nCollection Evidence\nLegal Coding\nLegal Content Support"
-        }
-      ];
+  const data = [
+    {
+      title: "Case Management",
+      details: [
+        "Reviewing, organizing, indexing, and summarizing client files",
+        "Preparing different correspondence",
+        "Preparing a variety of pleadings"
+      ]
+    },
+    {
+      title: "Discovery Preparation",
+      details: [
+        "Drafting Subpoenas, Interrogatories, and Discovery Demands",
+        "Detailed Analysis Reports of Depositions and Interrogatories",
+        "Witness Summary",
+        "Requesting for Medical Reports and Detailed Analysis of Medical Reports"
+      ]
+    },
+    {
+      title: "Trial Preparation",
+      details: [
+        "Expert witness report summarization",
+        "Document summarization for discovery",
+        "Property search from public records",
+        "Document Management with the use of software Casemap"
+      ]
+    },
+    {
+      title: "Legal Document Production",
+      details: [
+        "Contracts",
+        "Power of attorneys",
+        "Prenuptial agreements",
+        "Case Information statements",
+        "Property Settlement agreements"
+      ]
+    },
+    {
+      title: "Editorial Support",
+      details: [
+        "Editorial support to publishers who are looking for value-added enhancements to their publications. Our services comprise of:",
+        "Abstraction of case law and legal journals",
+        "Indexing legal material",
+        "Legal research",
+        "Copy editing"
+      ]
+    },
+    {
+      title: "Organizing Legal Database",
+      details: [
+        "Creating legal databases for law firms and corporates",
+        "Providing assistance in organizing the information into an index and searchable database"
+      ]
+    },
+    {
+      title: "Proofreading Services",
+      details: [
+        "Reviewing and rectifying spellings",
+        "Making grammatical changes if needed",
+        "Confirming uniformity in typefaces throughout the document",
+        "Determining accurate and consecutive numbering of several elements like page numbers",
+        "Looking for errors in formatting",
+        "Validating table of contents with elements like titles and subtitles",
+        "Validating the appropriateness of in-text illustrations",
+        "Real Estate Title Search",
+        "Data Entry Services"
+      ]
+    },
+    {
+      title: "Corporate Services",
+      details: [
+        "Coordinating Hearings, Depositions, and Meetings with Clients",
+        "Preparing Notices for Hearings and Depositions",
+        "Arranging Court Reporters and Ordering Transcripts",
+        "Formatting and Finalizing Pleadings and Correspondence",
+        "Filing Documents Electronically",
+        "Preparing Periodic Client Invoices",
+        "Performing Clerical Tasks and Assignments",
+        "Preparing Table of Authorities",
+        "Preparing Table of Contents",
+        "Legal Typing and Creating Minutes of Meetings"
+      ]
+    },
+    {
+      title: "Legal Research and Drafting Services",
+      details: [
+        "Performing Due Diligence",
+        "Data Room Management",
+        "Administrative and Regulatory Law Research Services",
+        "Legislation Research Services, including Primary (Regulations, Statutes, Cases, etc.) and Secondary Sources (Legal Dictionaries, Law Reviews, Treatises, etc.)",
+        "Case Law Research Services",
+        "Compliance Research Services",
+        "Multi-Jurisdictional Research Services",
+        "Memoranda of Law Research Services",
+        "Drafting of agreements/contracts as well as court-specific documents",
+        "Analyzing SEC Forms such as Form 4 and inputting the relevant information into Excel."
+      ]
+    },
+    {
+      title: "Legal Transcription Services",
+      details: [
+        "Transcribing audio files related to legal cases.",
+        "Tribunal Transcription",
+        "Verbatim Voice Dictation"
+      ]
+    },
+    {
+      title: "Litigation Support Services",
+      details: [
+        "Depositions",
+        "Medical Records",
+        "Document Review Management",
+        "Electronic Data Discovery Solutions",
+        "Processing of Information Stored Electronically"
+      ]
+    },
+    {
+      title: "Other services",
+      details: [
+        "Data Management",
+        "Reviewing documentation",
+        "Legal data entry services",
+        "Business registrations",
+        "Preparing Agreements and Contracts (business and other kind)",
+        "Data format conversion and storage",
+        "Collection Evidence",
+        "Legal Coding",
+        "Legal Content Support"
+      ]
+    }
+  ];
   
+
   return (
-     <>
-    
-
-      <div className="container mx-auto p-4 my-6 space-y-2 text-center">
-        <h2 className="text-3xl font-bold">
-        TLB’s Paralegal Services
-        </h2>
-        <p className="dark:text-gray-400">
-        We at The Legal Base understand the burden an attorney or a lawyer goes through. That is why we offer custom paralegal support services that are specially designed to suit your unique needs. Below mentioned are our portfolio of paralegal services that can be outsourced to us.
-
-
-        </p>
-      </div>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={"auto"}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 2,
-          slideShadows: true,
-        }}
-        pagination={true}
-        autoplay={{
-          delay: 1500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay,EffectCoverflow, Pagination]}
-        className="mySwiper"
-      >
-        {/* Step 2: Map the card data to the SwiperSlides */}
-        {cardData.map((card, index) => (
-          <SwiperSlide key={index}>
-            <div className="bg-white shadow-lg rounded-lg p-8">
-              <div className="flex items-center mb-6">
-                <div className="w-12 h-12 bg-blue-500 rounded-full flex items-center justify-center">
-                  <svg
-                    className="text-white w-6 h-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm0 2a10 10 0 100-20 10 10 0 000 20z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <h3 className="text-xl font-bold ml-4">{card.title}</h3>
+    <React.Fragment>
+      <div className="grid grid-cols-4 gap-4 m-4">
+        {data.map((item, index) => (
+          <div className="flip-card" key={index}>
+            <div className="flip-card-inner">
+              <div className="flip-card-front bg-white">
+                <h1 className="text-xl text-center justify-center mt-4">
+                  {item.title}
+                </h1>
+                <Image
+                  src="/management.png"
+                  className="justify-center mx-auto my-auto mt-2"
+                  width={100}
+                  height={100}
+                />
               </div>
-              <p className="text-gray-700">{card.description}</p>
+              <div className="flip-card-back overflow-auto">
+                <h1 className="text-xl text-center justify-center mt-4">
+                  {item.title}
+                </h1>
+                <ul className="list-decimal space-y-4 mt-2">
+                  {item.details.map((detail, index) => (
+                    <li key={index}>{detail}</li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          </SwiperSlide>
+          </div>
         ))}
-      </Swiper>
-
-
-      
-    </>
+      </div>
+    </React.Fragment>
   );
-}
+};
+
+export default Five;
