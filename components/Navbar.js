13db7c07cdf27d1b535426b8c10cb3001935e94d
Sumit pal <tonystark83033@gmail.com>
2023-07-05 16:26:41 +0530

    dark mode in mobile

--- components/Navbar.js
+++ components/Navbar.js
@@ -228,10 +228,10 @@ const Navbar = ({setTheam,theam}) => {
                   </div>
                   <button
                     type="button"
-                    onClick={toggleCart}
+                    onClick={handleTheam}
                     className="mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                   >
-                   Services
+{theam && (theam==="dark")? "Light" :"Dark"} 
                   </button>
                 </div>
               </div>
